92c392f827afee458b4b5be81f625c6729471c97
HninYeeMaungMaung <hninyemaungmaung@gmail.com>
2020-01-18 14:38:34 +0630

    Question D.2 commit

--- Singtel_TEST/src/main/java/com/main/fly/Butterfly.java
+++ Singtel_TEST/src/main/java/com/main/fly/Butterfly.java
@@ -7,10 +7,31 @@ import com.main.VolantAnimal;
  * QD.1
  *
  */
-public class Butterfly extends VolantAnimal{
+public class Butterfly extends VolantAnimal {
+
+	private String stage;
+
+	public Butterfly() {}
+
+	public Butterfly(String stage) {
+		this.stage = stage;
+	}
 
 	@Override
 	public void sing() {
 		throw new UnsupportedOperationException();
-	}	
+	}
+
+	@Override
+	public void fly() {
+		switch (stage) {
+		case "adult":
+			super.fly(); break;
+		case "immature":
+			throw new UnsupportedOperationException();
+		default : 
+			super.fly(); break;
+		}
+
+	}
 }
--- Singtel_TEST/src/test/java/TestFly.java
+++ Singtel_TEST/src/test/java/TestFly.java
@@ -13,7 +13,7 @@ public class TestFly {
 	public void testButterFly() {
 		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 		System.setOut(new PrintStream(outContent));
-		Butterfly butterfly = new Butterfly();
+		Butterfly butterfly = new Butterfly("adult");
 		butterfly.fly();
 		assertEquals("I am flying", outContent.toString());
 	}
@@ -23,5 +23,23 @@ public class TestFly {
 	public void testButterFlySing() {
 		Butterfly butterfly = new Butterfly();
 		butterfly.sing();
+	}
+
+
+	// D.2.a
+	@Test(expected = UnsupportedOperationException.class)
+	public void testCaterpillarFly() {
+		Butterfly caterpillar = new Butterfly("immature");
+		caterpillar.fly();
+	}
+
+	// D.2.b
+	@Test
+	public void testCaterpillarWalk() {
+		final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent));
+		Butterfly caterpillar = new Butterfly();
+		caterpillar.walk();
+		assertEquals("I am walking", outContent.toString());
 	}
 }
